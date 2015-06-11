94d2c084c2a064399f96369f171e73a7ff6f1605
cyvanoort <cyvan@live.nl>
2015-06-11 11:40:30 +0200

    logview copied to controller file

---
+++ src/main/webapp/js/controller/LogViewController.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+app.controller('LogViewController', function ($scope, Oboe) {
+
+    $scope.selected = null;
+    
+    $scope.setSelected = function(index){
+        $scope.selected = index;
+    }
+
+    $scope.files = [];
+        Oboe({
+           url: 'logbook',
+           pattern: '{file}'
+        }).then(function () {
+           // Finished loading
+        }, function (error) {
+           // handle errors
+           console.log(error);
+        }, function (node) {
+        // Node received
+        $scope.files.push(node);
+    });
+});
