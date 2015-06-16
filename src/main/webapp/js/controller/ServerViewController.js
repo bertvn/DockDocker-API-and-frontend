3f7ebce09da0549a20416dbe33bd2a34b715e935
ivanivanov <ivanivanov1992@gmail.com>
2015-06-16 17:08:33 +0200

    Add and delete server are working at the moment :dancer:

--- src/main/webapp/js/controller/ServerViewController.js
+++ src/main/webapp/js/controller/ServerViewController.js
@@ -2,6 +2,12 @@ app.controller('ServerViewController', function ($scope, Oboe, $http) {
 
     $scope.selected = null;
     $scope.selectedsServerID = null;
+    $scope.showForm = false;
+    
+    //Input value
+    $scope.ser_name = null;
+    $scope.ser_ip = null;
+    $scope.ser_doc_status = null;
 
     $scope.servers = [];
     
@@ -21,6 +27,30 @@ app.controller('ServerViewController', function ($scope, Oboe, $http) {
                 }).error(function () {
                     console.log('error');
                 });
+        }
+    }
+    
+    $scope.addServer = function () {
+        $scope.showForm = true;
+    }
+    
+    $scope.cancel = function() {
+        $scope.showForm = false;
+    }
+    
+    $scope.addServerToDB = function () {
+        //Check if all field are inserted
+        if($scope.ser_name && $scope.ser_ip && $scope.ser_doc_status != null ){
+            $http.get("addServer/" + $scope.ser_name + "/" + $scope.ser_ip + "/" + $scope.ser_doc_status).
+                success(function (response) {
+                    alert("The server is added into the database!");
+                    $scope.servers = [];
+                    callServerslist();
+                }).error(function () {
+                    console.log('error');
+                });
+        } else {
+            alert("You have empty field!");
         }
     }
 
