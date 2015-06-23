59d501adecc5fd832394c317ace5dd87bae7ea35
Patrick Aleman <alemanpatrick@gmail.com>
2015-06-23 21:24:43 +0200

    Added javadoc

--- src/main/webapp/js/controller/ContainerViewController.js
+++ src/main/webapp/js/controller/ContainerViewController.js
@@ -1,19 +1,34 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Angular controller for container view
+ *      Handels all containerview action
  */
 app.controller('ContainerViewController', function ($scope, Oboe) {
-
+    
+    // State variables
     $scope.selected = 0;
     $scope.showForm = false;
+    $scope.containers = [];
 
+    /**
+     * Updates $scope.selected with selected container information
+     * 
+     * @param index: index of container
+     * @param selectedID
+     */
     $scope.setSelected = function (index, selectedID) {
         $scope.selected = index;
         $scope.selectedsServerID = selectedID;
         $scope.showForm = false;
     };
-
+    
+    /**
+     * Executes container management tasks
+     * @param What (action):
+     *      * start: starts selected container
+     *      * stop: stops selected container
+     *      * restart: restarts selected container
+     * @param Id: container id
+     */
     $scope.executeTask = function (What, Id) {
         Oboe({
             url: "containers/" + Id + "/" + What,
@@ -28,30 +43,41 @@ app.controller('ContainerViewController', function ($scope, Oboe) {
         window.setTimeout(refresh(),10000);
     };
     
+    /**
+     * Refreshes the container information
+     */
     var refresh = function() {
         $scope.containers = [];
         getContainers();
     };
     
+    /**
+     * Shows the add container elements
+     */
     $scope.addContainer = function () {
         $scope.showForm = true;
         $scope.selected = null;
     }
 
-    $scope.containers = [];
+    /**
+     * Retrieves container information
+     * appends container information to list with containers
+     */
     var getContainers = function() {
-    Oboe({
-        url: 'containerslist',
-        pattern: '{Id}'
-    }).then(function () {
-        // Finished loading
-    }, function (error) {
-        // handle errors
-        console.log(error);
-    }, function (node) {
-        // Node received
-        $scope.containers.push(node);
-    });
+        Oboe({
+            url: 'containerslist',
+            pattern: '{Id}'
+        }).then(function () {
+            // Finished loading
+        }, function (error) {
+            // handle errors
+            console.log(error);
+        }, function (node) {
+            // Node received
+            $scope.containers.push(node);
+        });
     };
+    
+    // Called on load
     getContainers();
 });
